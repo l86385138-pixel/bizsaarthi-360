ea6623a4ec7bfcd46149265adfd27c5d7fe90b7e
l86385138-pixel <l86385138@gmail.com>
2026-09-10 17:29:43 +0530

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,11 @@
+// BizSaarthi 360 - Firebase Configuration
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyD_I1wITrxbC_ia5e5EDAvPF438wiQ_gLY",
+  authDomain: "bizsaarthi-360.firebaseapp.com",
+  projectId: "bizsaarthi-360",
+  storageBucket: "bizsaarthi-360.firebasestorage.app",
+  messagingSenderId: "260401304810",
+  appId: "1:260401304810:web:1af53382935058074fb393",
+  measurementId: "G-CRMQ93KS1D"
+};
